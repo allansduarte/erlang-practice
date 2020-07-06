eaab187b936a4ad9d0a87977198219d53330ff23
Allan <allan.sduarte@gmail.com>
2020-07-06 17:36:40 -0300

    Refactor Two Fer solution

--- two-fer/src/two_fer.erl
+++ two-fer/src/two_fer.erl
@@ -3,8 +3,8 @@
 -export([two_fer/0, two_fer/1]).
 
 two_fer() ->
-    "One for you, one for me.".
+    two_fer("you").
 
-two_fer(_Name) ->
-    "One for " ++ _Name ++ ", one for me.".
+two_fer(Name) ->
+    "One for " ++ Name ++ ", one for me.".
 
